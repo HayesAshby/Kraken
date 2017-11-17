ae45436cbbd0ad478990a485277d9b102fa38810
Zachary Neubert <zacneubert@gmail.com>
2017-11-17 16:45:27 -0700

    Butterknife and added google signin

--- app/src/main/java/com/example/hayzbomb/kraken/GoogleSignin.java
+++ app/src/main/java/com/example/hayzbomb/kraken/GoogleSignin.java
@@ -1,14 +1,29 @@
-package com.kraken.standalone;
+package com.example.hayzbomb.kraken;
 
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.kraken.standalone.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class GoogleSignin extends AppCompatActivity {
+
+    @BindView(R.id.button) Button button;
+
+    @OnClick(R.id.button)
+    public void click() {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_signin);
+        ButterKnife.bind(this);
     }
 }
